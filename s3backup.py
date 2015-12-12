b8af5e954b4099bd69eeda0e018606f3773eff23
rlarsen <nonades@gmail.com>
2015-12-12 16:46:48 -0500

    Check existence of file to backup

--- s3backup.py
+++ s3backup.py
@@ -1,8 +1,20 @@
-import boto, sys
+import boto, sys, getopt, os.path
 
-check_args(argv):
+def check_file(backup_file):
+    if not os.path.isfile(backup_file):
+        print "To backup file doesn't exist"
+        sys.exit(2)
+
+def check_args(argv):
+    usage = "Usage: s3backup.py -b <bucket name> -f <file to backup"
     try:
-        args, opts = getopt.getopt(argv, "b:f:", ["bucket", "file"])
+        opts, args = getopt.getopt(argv, "b:f:")
+    except getopt.GetoptError:
+        print usage
+        sys.exit(2)
+    return opts
 
 if __name__ == '__main__':
-    check_args(sys.argv[1:])
+    args = check_args(sys.argv[1:])
+
+    check_file(args[1][1])
