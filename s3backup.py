e57bfacbf4fc4e949b87805d3a6c261230126b5c
rlarsen <nonades@gmail.com>
2015-12-14 11:16:18 -0500

    Commented my code

--- s3backup.py
+++ s3backup.py
@@ -1,24 +1,30 @@
 import boto, sys, getopt, os.path
 
+#sets up s3 connection
 def connect_s3():
    s3 = boto.connect_s3()
    return s3
 
+#Checks for existence of file to backup
 def check_file(backup_file):
     if not os.path.isfile(backup_file):
         print "To backup file doesn't exist"
         sys.exit(2)
 
+#Checks existence of s3 bucket to backup to
+#TODO: Add support for creating a bucket
 def check_bucket(conn, bucket):
     test = conn.lookup(bucket, validate=True)
     if test is None:
         print "Bucket does not exist"
         sys.exit(2)
-	
+
+#Gets s3 bucket object
 def get_bucket(conn, bucket):
     bucket_obj = conn.get_bucket(bucket)
     return bucket_obj
 
+#uploads named file to s3 bucket and sets permissions
 def add_file(s3, bucket, to_backup):
     key = bucket.get_key(to_backup)
 
@@ -27,6 +33,7 @@ def add_file(s3, bucket, to_backup):
         key.set_contents_from_filename(to_backup)
         key.set_acl('public-read')
 
+#Verifies you're using the script correctly
 def check_args(argv):
     usage = "Usage: s3backup.py -b <bucket name> -f <file to backup"
     try:
@@ -36,6 +43,7 @@ def check_args(argv):
         sys.exit(2)
     return opts
 
+#Main flow control
 if __name__ == '__main__':
     args = check_args(sys.argv[1:])
     conn = connect_s3()
